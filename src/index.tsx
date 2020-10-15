4b37a95b46f70c4d6cdc63a0277cc0c5675028ef
Vignesh Joglekar <rexvigasaurus@gmail.com>
2020-10-15 17:20:03 -0500

    Unregister old serviceworkers + Netlify Headers

--- src/index.tsx
+++ src/index.tsx
@@ -2,5 +2,8 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import './css/built.css';
 import App from './App';
+import { unregister } from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('app'));
+
+unregister();
